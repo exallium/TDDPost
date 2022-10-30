43653f58b519d8bcb2e5929eb28c0fc96ec95ba7
Alex Hart <alex.hart658@gmail.com>
2022-10-30 13:13:24 -0300

    First test passes.

--- src/main/kotlin/TodoPresenter.kt
+++ src/main/kotlin/TodoPresenter.kt
@@ -3,4 +3,11 @@
  */
 class TodoPresenter(todoRepository: TodoRepository) {
   val notes: List<TodoNote> = todoRepository.getNotes()
+    .sortedWith(NoteComparator().reversed())
+
+  private class NoteComparator : Comparator<TodoNote> {
+    override fun compare(o1: TodoNote, o2: TodoNote): Int {
+      return o1.creationTimestamp.compareTo(o2.creationTimestamp)
+    }
+  }
 }
